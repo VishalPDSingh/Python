783ccceb8763d009650983635f8935feae9efc91
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-06 16:45:13 +0530

    first commit

--- Ch2_Operator/Arithmetic.py
+++ Ch2_Operator/Arithmetic.py
@@ -15,7 +15,8 @@ print("Sub is ",(a-b))
 print("Mul is ",(a*b))
 print("div is ",(a/b)) # divsion always gives the floating number, if we want witho floating value then use floor divison(//)
 print("mod is ",(a%b))
-print("div is ",(a//b))
+print("Floor Division is ",(a//b)) # it give int value 
 print("Power is ",a**b)
 # in the expression pecedence of operataor is
 # first (), secnond **, third *,/, //, % from left to right, fourth +,-
+print(5+2*3-1+10/5)
