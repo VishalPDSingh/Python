f800ac4bfa984ceffa3ac2644cc30a22b5bbb6d2
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-07 19:33:40 +0530

    first commit

---
+++ Ch2_Operator/MemberShipOp.py
@@ -0,0 +1,9 @@
+str = "vishal"
+print("y" in str)
+# it check in the sequnces the value is present in or not
+print("i" in str)
+print("al" in str)
+print("z" not in str)
+
+l = [12,12,65,6,47,9,12,38,12,10]
+print(10 in l)
