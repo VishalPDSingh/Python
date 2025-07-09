69aa588c1513e7febe9335180ae792d558c5588f
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-09 11:47:04 +0530

    first commit

---
+++ Ch4_LoopIn_Py/CodingTask1.py
@@ -0,0 +1,15 @@
+height = input("Enter the height sepated by a space: ")
+height_list= height.split()
+print(height_list)
+count = 0
+for height in height_list:
+    count = count+1
+print(count)
+for i in range(count):
+    height_list[i] = int(height_list[i])
+total = 0
+for person in height_list:
+    total+= person
+avg = total/count
+print(round(avg))
+#print(height_list)
---
+++ Ch4_LoopIn_Py/WhileLoop.py
@@ -0,0 +1,7 @@
+# while condition/expression
+    #statements
+
+count = 1
+while count<=5:
+    print(count)
+    count+=1
