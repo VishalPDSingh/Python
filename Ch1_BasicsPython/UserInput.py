0e7f0ceee92e8927c43bd66c57396d9d6900f802
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-10 11:23:24 +0530

    first commit

--- Ch1_BasicsPython/UserInput.py
+++ Ch1_BasicsPython/UserInput.py
@@ -2,8 +2,7 @@
 #input("What is your name ")
 print("Hello "+ " "+input("What is your name: ")) # this is nested function, first input() function is excutes the concatenation
 
-
 #Task 
 # input("What is your name: ")
 # print("Hey Vishal, How are you ") wrong way 
-print("Hey "+input("What is your name: ")+"How are you? ") # correct way 
+print("Hey "+input("What is your name: ")+" How are you? ") # correctway
