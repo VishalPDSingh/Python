63f45ad6d6ab66c89f4f4a82707e8db53da3c343
VishalPDSingh <vishalprasadsingh52@gmail.com>
2025-07-14 12:18:59 +0530

    commit

--- Ch4_LoopIn_Py/For_ElseLoop.py
+++ Ch4_LoopIn_Py/For_ElseLoop.py
@@ -7,8 +7,10 @@
 tuple1 = (2,56,65,4,6,37)
 for i in tuple1:
     print(i)
-    # if(i==4):
-    #     break
+    if(i==4):
+        break
 else:
     print("Completed the code")
 print("out of loop")
+
+
